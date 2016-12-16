a8525b6250386c0e3d5a3053daaf8e30ae0806a9
Arnau Dillen <arnau.dillen@hotmail.com>
2016-12-16 18:54:48 +0100

    Created file for SPARQL engine

---
+++ allergies/public/javascripts/sparql-engine.js
@@ -0,0 +1,16 @@
+sparql = require 'sparql'
+var rdfstore = require('rdfstore');
+
+dbpedia = new sparql.Client 'http://dbpedia.org/sparql'
+foodExt = new sparql.Client ''//TODO: add external data set
+
+
+//Example query
+// client.query 'select * where { ?s ?p ?o } limit 100', (err, res) ->
+//   console.log res
+
+//Function that takes a string representing a SPARQL query to our endpoint and returns the results
+function handleQuery(query){
+	//Handle query and access rdfstore here
+
+}
